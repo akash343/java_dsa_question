cc091a9945646b68d4298d88e88af0ce99d201b1
akash343 <95898158+akash343@users.noreply.github.com>
2022-09-18 17:25:25 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,12 @@
+public class aa{
+public static int multiply(int a,int b){
+    int product = a*b ;
+    return product;
+}
+public static void main(String args[]){
+    int a =5;
+    int b = 6;
+   int product = multiply(a,b);
+   System.out.println("a*b="+product);
+}
+}
